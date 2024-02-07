0e7e9de9f8011f6b5df790225c4d9a01eede51d2
badr <badr4boukries@gmail.com>
2024-02-07 17:05:27 +0100

    delete location1

--- Location1-service/src/main/java/com/example/location1service/repository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.location1service.repository;
-
-import com.example.locationservice.entities.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location,Integer> {
-}
